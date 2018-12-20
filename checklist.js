ab825c3202f74fa0cbc4c1c22b26e7e3eb1f2a1d
Willa Jin <jin.willa@gmail.com>
2018-12-19 21:45:58 -0800

    updated checklist and added firebase

--- checklist.js
+++ checklist.js
@@ -1,17 +1,19 @@
-// define global variables
-//localStorage.getItem("task") ? toDoArray = JSON.parse(localStorage.getItem("task")) : toDoArray = [];
-// to do task array
-var toDoArray = (localStorage.getItem("task")) ? JSON.parse(localStorage.getItem("task")) : toDoArray = [];
-console.log(toDoArray);
+// initialize firebase
 
-// done task array
-var doneArray = (localStorage.getItem("done")) ? JSON.parse(localStorage.getItem("done")) : doneArray = [];
-console.log(doneArray);
+var config = {
+    apiKey: "AIzaSyBazcPqU9V_MbRsEHtIk3oFvHHtGSwS2l8",
+    authDomain: "project-aef34.firebaseapp.com",
+    databaseURL: "https://project-aef34.firebaseio.com",
+    projectId: "project-aef34",
+    storageBucket: "project-aef34.appspot.com",
+    messagingSenderId: "668614696225"
+};
+firebase.initializeApp(config);
 
-// update display
-displayList();
+// create firebase variable
+var database = firebase.database();
 
-// on button click - add task
+// on addButton click - add to firebase 
 $("#addButton").on("click", function (event) {
     event.preventDefault();
 
@@ -20,126 +22,124 @@ $("#addButton").on("click", function (event) {
     var dateInput = $("#due-date").val().trim();
     var noteInput = $("#notes").val().trim();
 
-    // add if task input has value
+    // store in firebase if task input has value
     if (taskInput) {
-        addTask(taskInput, dateInput, noteInput);
+        database.ref().push({
+            task: taskInput,
+            dueDate: dateInput,
+            notes: noteInput,
+            done: "false"
+        });
     }
+
+    else {
+        swal("Please enter a task!");
+    }
+
+    // clear form
+    $("#task").val("");
+    $("#due-date").val("");
+    $("#notes").val("");
+
 });
 
-// on enter button press - add task
-$(document).keypress(function (event) {
-    // get user input
-    var taskInput = $("#task").val().trim();
-    var dateInput = $("#due-date").val().trim();
-    var noteInput = $("#notes").val().trim();
+// firebase event to add task to table
+database.ref().on("child_added", function (snapshot) {
+    console.log(snapshot.val());
+    console.log(snapshot.key);
 
-    // add task if enter key is pressed and task input has value
-    if (event.which === 13 && taskInput) {
-        addTask(taskInput, dateInput, noteInput);
-    }
+    // get data from firebase
+    var taskInput = snapshot.val().task;
+    var dateInput = snapshot.val().dueDate;
+    var noteInput = snapshot.val().notes;
+
+    // create done button
+    var doneButton = $("<button>");
+    //doneButton.attr("data-done", i);
+    doneButton.addClass("doneButton btn-floating btn-small");
+    doneButton.html("<i class='material-icons'>check</i>");
+
+    // create delete button
+    var deleteButton = $("<button>");
+    //deleteButton.attr("data-delete", i);
+    deleteButton.addClass("deleteButton btn-floating btn-small");
+    deleteButton.html("<i class='material-icons'>delete</i>");
+
+    // create new table row
+    var newRow = $("<tr>").append(
+        $("<td>").text(taskInput),
+        $("<td>").text(dateInput),
+        $("<td>").text(noteInput),
+        $("<td>").append(doneButton),
+        $("<td>").append(deleteButton)
+    )
+
+    // add firebase key to row
+    newRow.attr("data-key", snapshot.key);
+
+    // append row to toDo table
+    $("#toDoTable").append(newRow);
 });
 
 // on delete button click - delete task
 $(document).on("click", ".deleteButton", deleteTask);
 
-// on done button click - remove from to do table and add to done table
-$(document).on("click", ".doneButton", completeTask);
-
-// add task
-function addTask(taskInput, dateInput, noteInput) {
-    var taskObject = {
-        task: taskInput,
-        duedate: dateInput,
-        note: noteInput
-    };
-
-    // push to toDo array
-    toDoArray.push(taskObject);
-    console.log(toDoArray);
-
-    // store in localStorage
-    localStorage.setItem("task", JSON.stringify(toDoArray));
-
-    // clear inputs
-    $("#task").val("");
-    $("#due-date").val("");
-    $("#notes").val("");
-
-    // update display
-    displayList();
-}
-
-// display to do list
-function displayList() {
-    // update to do table
-    for (var i = 0; i < toDoArray.length; i++) {
-        
-        // create done button
-        var doneButton = $("<button>");
-        doneButton.attr("data-done", toDoArray[i]);
-        doneButton.addClass("doneButton btn-floating btn-small");
-        doneButton.html("<i class='material-icons'>check</i>");
-
-        // create delete button
-        var deleteButton = $("<button>");
-        deleteButton.attr("data-delete", toDoArray[i]);
-        deleteButton.addClass("deleteButton btn-floating btn-small");
-        deleteButton.html("<i class='material-icons'>delete</i>");
-
-        var toDoRow = $("<tr>").append(
-            $("<td>").text(toDoArray[i].task),
-            $("<td>").text(toDoArray[i].duedate),
-            $("<td>").text(toDoArray[i].note),
-            $("<td>").append(doneButton),
-            $("<td>").append(deleteButton)
-        )
-    }
-
-    // update done table
-    for (var i = 0; i < doneArray.length; i++) {
-        var doneRow = $("<tr>").append(
-            $("<td>").text(doneArray[i][0].task),
-            $("<td>").text(doneArray[i][0].duedate),
-            $("<td>").text(doneArray[i][0].note)
-        )
-    }
-
-    // append row to tables
-    $("#toDoTable").append(toDoRow);
-    $("#doneTable").append(doneRow);
-}
-
 // delete task
 function deleteTask() {
-    // remove from to do table
-    $(this).closest("tr").remove();
+    swal("Delete this task?", {
+        buttons: ["Cancel", "Delete"],
+    }).then((willDelete) => {
+        if (willDelete) {
+            // get row of task
+            var taskRow = $(this).closest("tr");
 
-    // remove from array
-    var toDoNumber = $(this).attr("data-delete");
-    toDoArray.splice(toDoNumber, 1);
-    console.log(toDoArray);
+            // remove from table
+            taskRow.remove();
 
-    // localStorage
-    localStorage.setItem("task", JSON.stringify(toDoArray));
+            // get key of row
+            var taskKey = taskRow.attr("data-key");
+            console.log("taskKey: " + taskKey);
+
+            // delete row from firebase
+            database.ref().child(taskKey).remove();
+        }
+    });
 }
 
+// on done button click - complete task
+$(document).on("click", ".doneButton", completeTask);
+
 // complete task
-function completeTask() {    
-    // remove from toDoArray
-    var toDoNumber = $(this).attr("data-delete");
-    var x = toDoArray.splice(toDoNumber, 1);
+function completeTask() {
+    // get row of task
+    var taskRow = $(this).closest("tr");
 
-    // remove from to do table
-    $(this).closest("tr").remove();
+    // get key of row
+    var taskKey = taskRow.attr("data-key");
+    console.log("taskKey: " + taskKey);
 
-    // add to doneArray
-    doneArray.push(x);
-    console.log("toDo: ", toDoArray);
-    console.log("done: ", doneArray);
-    
-    // store in localStorage
-    localStorage.setItem("done", JSON.stringify(doneArray));
+    database.ref().child(taskKey).once("value", function (snap) {
+        var taskDone = snap.val().done;
+        console.log("taskDone: " + taskDone);
 
-    // update display
-    displayList();
+        // check if task is not done, then set task to done and update css
+        if (taskDone === "false") {
+            // update css to done
+            taskRow.css({ "color": "lightgray", "font-style": "italic" });
+
+            // set taskDone to true
+            database.ref(taskKey).update({done: "true"});
+            var taskDone = "true";
+        }
+
+        // else if task is done, set task to not done
+        else {
+            // update css to not done
+            taskRow.css({ "color": "black", "font-style": "normal" });
+            
+            // set taskDone to true
+            database.ref(taskKey).update({done: "false"});
+            var taskDone = "false";
+        }
+    });
 }
